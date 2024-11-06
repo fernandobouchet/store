2b45dcb977f1f3b72c59e66e5b79a279f3df3e7c
fernandobouchet <fernandobouchet@gmail.com>
2024-11-06 20:40:59 -0300

    feat: add 'drawer' component

---
+++ components/ui/drawer.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@nextui-org/react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+interface Props {
+  isCartOpen: boolean;
+  setIsCartOpen: Dispatch<SetStateAction<boolean>>;
+  title: string;
+  children?: React.ReactNode;
+}
+
+export default function Drawer({
+  isCartOpen,
+  setIsCartOpen,
+  title,
+  children,
+}: Props) {
+  const [isVisible, setIsVisible] = useState(isCartOpen);
+
+  const onClose = () => {
+    setIsVisible(false);
+    setTimeout(() => setIsCartOpen(false), 300);
+    document.body.style.overflow = "";
+  };
+
+  useEffect(() => {
+    if (isCartOpen) {
+      setIsVisible(true);
+      document.body.style.overflow = "hidden";
+    }
+  }, [isCartOpen]);
+
+  if (!isCartOpen && !isVisible) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 h-screen overflow-hidden">
+      <div className="absolute inset-0" onClick={onClose} aria-hidden="true" />
+      <div
+        className={`fixed right-0 top-0 min-h-screen w-full max-w-sm bg-background shadow-xl transform transition-transform duration-300 ease-in-out overflow-hidden ${
+          isVisible ? "translate-x-0 overflow-hidden" : "translate-x-full"
+        }`}
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="side-menu-title"
+      >
+        <div className="flex flex-col h-full">
+          <div className="flex items-center justify-between p-4">
+            <h2 id="side-menu-title" className="text-lg font-semibold">
+              {title}
+            </h2>
+            <Button
+              isIconOnly
+              variant="light"
+              onClick={onClose}
+              aria-label="Cerrar menú"
+            >
+              <IoClose className="h-6 w-6" />
+            </Button>
+          </div>
+          <div className="flex-grow overflow-y-auto p-4 h-full bg-background">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
