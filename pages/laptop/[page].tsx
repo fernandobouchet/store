03c9d4b074c52015a5dc75d962fd1e975c9a822e
fernandobouchet <fernandobouchet@gmail.com>
2024-11-10 00:56:03 -0300

    feat: update number of pregenerated pages

--- pages/laptop/[page].tsx
+++ pages/laptop/[page].tsx
@@ -78,7 +78,7 @@ export const getStaticProps: GetStaticProps = async ({
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: Array.from({ length: 5 }).map((_, i) => ({
+    paths: Array.from({ length: 2 }).map((_, i) => ({
       params: { page: (i + 2).toString() },
     })),
     fallback: "blocking",
